a05692e04812657acb926dcc341cfcd51b31df39
Matheus Lopes de Sousa <mmatheuslsousa@gmail.com>
2025-04-01 22:15:43 -0300

    Descrição das alterações feitas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
-inport { box } from '@mui/material';
+import {Box,Card,Typorgraphy } from '@mui/material';
 
 export default function App() {
 
   return (
-        <box>
-          TESTE
-        </box>
-  )
+        <Box
+          display={'flex'}
+          justifyContent={'center'}
+          alignItems={'flex-start'}
+          width={'100vw'}
+          height={'100vh'}
+          //paddingX={'16px'}
+          bgcolor={'red'}
+        >
+          <Card
+            sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            alignItems: 'center',
+            width: '100%',
+            marginTop: '146px'
+          }}  
+          >
+       
+        {/* Titulo */}
+       <Box>
+        <Typorgraphy Variant='h5' fontWeight={'700'}>
+
+        </Typorgraphy>
+        
+        calculadora imc 
+       
+       </Box>
+     
+     {/* Entrada */} 
+     <Box>
+
+     </Box>
+
+     {/* saida */}
+     <Box>
+
+     </Box>
+
+<Card/> 
+</Box>
+
+    )
 }
+
+
+
